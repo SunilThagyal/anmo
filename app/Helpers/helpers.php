efb7844a59865f0e2fcb1bb548161bff72d24a23
SunilThagyal <sunilthagyal355@gmail.com>
2023-10-29 02:02:04 +0530

    fixes

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,47 @@
+<?php
+use GuzzleHttp\Client;
+use Jenssegers\Agent\Facades\Agent;
+
+
+function userInformation(){
+    $device = Agent::device();
+    $browser = Agent::browser();
+    $version = Agent::version($browser);
+    $platform = Agent::platform();
+    $userIP = request()->ip();
+    $userIP = "38.137.44.208";
+    /* device type */
+$deviceType = match(true) {
+    Agent::isDesktop() => "desktop",
+    Agent::isTablet() => "tablet",
+    Agent::isPhone() => "phone",
+    default => "unknown"
+};
+/* get location using ip */
+$client = new Client();
+$response = $client->get("https://ipinfo.io/{$userIP}?token=".config('constants.ip_info_token'));
+$locationData = json_decode($response->getBody());
+$country = $locationData->country;
+$city = $locationData->city;
+$loc = $locationData->loc;
+$postal_code = $locationData->postal;
+$service_used = $locationData->org;
+$time_zone = $locationData->timezone;
+$deviceID = uniqid($platform);
+$user_data =[
+    'ip' => $userIP,
+    'country' => $country,
+    'city' => $city,
+    'loc' => $loc,
+    'postal_code' => $postal_code,
+    'service_used' => $service_used,
+    'time_zone' => $time_zone,
+    'device_id' => $deviceID,
+    'device_type' => $deviceType,
+    'device' => $device,
+    'browser' => $browser,
+    'platform' => $platform
+];
+$user_data_json = serialize($user_data);
+return $user_data_json;
+}
